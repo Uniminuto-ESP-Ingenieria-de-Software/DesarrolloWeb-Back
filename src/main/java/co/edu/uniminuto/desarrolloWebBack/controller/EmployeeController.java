84d16f5e3ae8245bf15db058bc6c8f6be7041f94
hvigoya <ing.hugovigoya@gmail.com>
2023-10-10 05:44:19 -0500

    Backend final

--- src/main/java/co/edu/uniminuto/desarrolloWebBack/controller/EmployeeController.java
+++ src/main/java/co/edu/uniminuto/desarrolloWebBack/controller/EmployeeController.java
@@ -10,21 +10,26 @@ import org.springframework.web.bind.annotation.*;
 public class EmployeeController {
     @Autowired
     private IEmployeeService service;
+
+    @CrossOrigin(origins = "*")
     @GetMapping("/api/employees")
     public List<Employee> getAll() {
         return service.getAll();
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/api/employees/{id}")
     public Employee getById(@PathVariable String id) {
         return service.getById(Integer.parseInt(id));
     }
 
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/api/employees/{id}")
     public void remove(@PathVariable String id) {
         service.remove(Integer.parseInt(id));
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/api/employees")
     public void register(@RequestBody Employee employee) {
         service.register(employee);
